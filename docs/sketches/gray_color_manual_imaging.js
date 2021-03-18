407599212389a0a3116a1167274d000f5892937c
ccgomezn <cristhian@voice123.com>
2021-03-18 14:28:05 +0000

    Deploying to gh-pages from  @ bcd1523c1195accefc4443416b1acbec31fa2a25 🚀

--- docs/sketches/gray_color_manual_imaging.js
+++ docs/sketches/gray_color_manual_imaging.js
@@ -9,19 +9,18 @@ function setup() {
 function draw() {
   image(img, 0, 0, 512, 512);
   loadPixels();
-  for (let y = 0; y < 512; y++) {
-      for (let x = 0; x < 512; x++) {
-        let index = (x + y * width)*4;
-        let r = pixels[index+0];
-        let g = pixels[index+1];
-        let b = pixels[index+2];
-        
-        let gray = r *.299 + g *.587 + b *.0114;
-        
-        pixels[index+0] = gray;
-        pixels[index+1] = gray;
-        pixels[index+2] = gray;
-    }
+  let d = pixelDensity();
+  let fullImage = (width * d) * (height * d);
+  for (let i = 0; i < fullImage; i++) {
+    let r = pixels[i*4];
+    let g = pixels[i*4+1];
+    let b = pixels[i*4+2];
+    
+    let gray = r *.299 + g *.587 + b *.0114;
+    
+    pixels[i*4] = gray;
+    pixels[i*4+1] = gray;
+    pixels[i*4+2] = gray;
   }
   updatePixels();
 }
