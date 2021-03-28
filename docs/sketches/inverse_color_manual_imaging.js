4ed74112790bfa5bf56db2420c98d31ec3124cd7
ccgomezn <cristhian@voice123.com>
2021-03-28 16:24:04 +0000

    Deploying to gh-pages from  @ 8a22213d567ae2082e2df686f8a6d8e3ab5fba2b 🚀

--- docs/sketches/inverse_color_manual_imaging.js
+++ docs/sketches/inverse_color_manual_imaging.js
@@ -1,13 +1,17 @@
 function preload() {
   img = loadImage('/vc/docs/sketches/lenna.png');
+  video = createVideo(['/vc/docs/sketches/fingers.mov', '/vc/docs/sketches/fingers.webm']);
+  video.hide();
 }
 
 function setup() {
-  createCanvas(512, 512);
+  createCanvas(512, 256);
 }
 
 function draw() {
-  image(img, 0, 0, 512, 512);
+  image(img, 0, 0, 256, 256);
+  
+  image(video, 256, 0, 256, 256);
   loadPixels();
   let d = pixelDensity();
   let fullImage = (width * d) * (height * d);
