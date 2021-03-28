f9ed68bc194058ef445af28f2a218ec76fdefaab
ccgomezn <cristhian@voice123.com>
2021-03-28 16:16:08 +0000

    Deploying to gh-pages from  @ b25b2f3238d55efb28d842cc7c596763156446bb 🚀

--- docs/sketches/imaging_top.js
+++ docs/sketches/imaging_top.js
@@ -1,4 +1,4 @@
-let imaging_top;
+let imaging;
 
 function preload() {
   img_or = loadImage('/vc/docs/sketches/lenna.png');
@@ -14,25 +14,74 @@ function preload() {
 
 function setup() {
   createCanvas(768, 512);
-  img_gr.filter(GRAY)
-  img_inv.filter(INVERT)
-   
+  img_inv.loadPixels();
+  let d = pixelDensity();
+  let fullImage = (width * d) * (height * d);
+  for(let i=0; i < fullImage; i++){
+    let r = img_inv.pixels[i*4];
+    let g = img_inv.pixels[i*4+1];
+    let b = img_inv.pixels[i*4+2];
 
+    img_inv.pixels[i*4] = 255-r;
+    img_inv.pixels[i*4+1] = 255-g;
+    img_inv.pixels[i*4+2] = 255-b;
+  }
+  img_inv.updatePixels();
+
+  img_gr.loadPixels();
+  d = pixelDensity();
+  fullImage = (width * d) * (height * d);
+  for(let i=0; i < fullImage; i++){
+    let r = img_gr.pixels[i*4];
+    let g = img_gr.pixels[i*4+1];
+    let b = img_gr.pixels[i*4+2];
+    let gray = r *.299 + g *.587 + b *.0114;
+
+    img_gr.pixels[i*4] = gray;
+    img_gr.pixels[i*4+1] = gray;
+    img_gr.pixels[i*4+2] = gray;
+  }
+  img_gr.updatePixels();
   fingers_or.loop();
   fingers_gr.loop();
   fingers_inv.loop();
 }
 
 function draw() {
-
-
-  image(fingers_gr, 256, 256, 256, 256);
-  filter(GRAY);
-  image(fingers_inv, 512, 256, 256, 256);
-  filter(INVERT);
-  image(fingers_or, 0, 256, 256, 256);
   image(img_or, 0, 0, 256, 256);
   image(img_gr, 256, 0, 256, 256);
   image(img_inv, 512, 0, 256, 256);
 
+  image(fingers_or, 0, 256, 256, 256);
+  image(fingers_gr, 256, 256, 256, 256);
+  image(fingers_inv, 512, 256, 256, 256);
+
+  fingers_gr.loadPixels();
+  d = pixelDensity();
+  fullImage = (width * d) * (height * d);
+  for(let i=0; i < fullImage; i++){
+    let r = fingers_gr.pixels[i*4];
+    let g = fingers_gr.pixels[i*4+1];
+    let b = fingers_gr.pixels[i*4+2];
+    let gray = r *.299 + g *.587 + b *.0114;
+
+    fingers_gr.pixels[i*4] = gray;
+    fingers_gr.pixels[i*4+1] = gray;
+    fingers_gr.pixels[i*4+2] = gray;
+  }
+  fingers_gr.updatePixels();
+
+  fingers_inv.loadPixels();
+  d = pixelDensity();
+  fullImage = (width * d) * (height * d);
+  for(let i=0; i < fullImage; i++){
+    let r = fingers_inv.pixels[i*4];
+    let g = fingers_inv.pixels[i*4+1];
+    let b = fingers_inv.pixels[i*4+2];
+
+    fingers_inv.pixels[i*4] = 255-r;
+    fingers_inv.pixels[i*4+1] = 255-g;
+    fingers_inv.pixels[i*4+2] = 255-b;
+  }
+  fingers_inv.updatePixels();
 }
--- docs/sketches/original_imaging.js
+++ docs/sketches/original_imaging.js
@@ -1,11 +1,15 @@
 function preload() {
   img = loadImage('/vc/docs/sketches/lenna.png');
+  video = createVideo(['/vc/docs/sketches/fingers.mov', '/vc/docs/sketches/fingers.webm']);
+  video.hide();
 }
 
 function setup() {
-  createCanvas(512, 512);
+  createCanvas(512, 256);
+  video.loop();
 }
 
 function draw() {
-  image(img, 0, 0, 512, 512);
+  image(img, 0, 0, 256, 256);
+  image(video, 256, 0, 256, 256);
 }
