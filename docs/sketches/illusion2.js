24b63de161fa7f7f39b8914d1e7d35b577d0e7d1
ccgomezn <cristhian@voice123.com>
2021-04-03 01:52:50 +0000

    Deploying to gh-pages from  @ 4e4364dfe68775dec2ae9f0a4be9d875f31def10 🚀

---
+++ docs/sketches/illusion2.js
@@ -0,0 +1,16 @@
+let imaging;
+
+function setup() {
+  createCanvas(300, 300);
+  
+}
+
+function draw() {
+  ellipse(150, 300, 150, 150);
+  noFill();
+  ellipse(100, 300, 100, 100);
+  noFill();
+    line(0,300,300,300)
+    line(150,0,150,300)
+  
+}
