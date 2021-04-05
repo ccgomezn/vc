2e5513771d10910c34f2a1f2b8835ef5b5c95d70
ccgomezn <cristhiancamilogomezneira@Cristhians-MacBook-Pro.local>
2021-04-04 19:22:15 -0500

    full

--- docs/sketches/ascii_art.js
+++ docs/sketches/ascii_art.js
@@ -1,56 +1,45 @@
-var cnv,img;
-var resdiv;
-var options = [' ','`','.',',-',"':",';_~','"','*|','!l',
-'+=','>','<L','\\i','/^','1?','Jv','r','()cx','7}','sz',
-'3u','2Ckty{','jn','4FVY','5P[]af','qw','Sde','Eo',
-'NOZ','9HXgh','GTU','$AIm','QW','KM','%8','#06@','bp',
+let img;
+let div;
+var options = [' ','`','.','-',":",';','"','*','!',
+'=','>','L','i','/','?','J','r','x','7','z',
+'3','C','n','F','P','w','S','E',
+'N','9','T','$','Q','M','%','@','b',
 'D','&','R','B'];
-var gui,btn,livebtn;
-var live = false;
-var capture;
-var pg;
+function preload() {
+  img = loadImage('/vc/docs/sketches/lenna.png');
+}
 
 function setup() {
-  //paragraph for display of ascii result
-  resdiv = createP('');
-  
-  //video capture elements
-  pg = createGraphics(160,120);
-  capture = createCapture(VIDEO);
-  capture.size(80, 60);
-  capture.hide();
-  
-  //gui elements
-  btn = createButton('UPDATE');
-  btn.mousePressed(calcCapture);
-  gui = createDiv('');
-  btn.parent(gui);
-  livebtn = createButton('LIVE');
-  livebtn.mousePressed(function(){live=!live;});
-  livebtn.parent(gui);
-  
-  cnv = createCanvas(400,300);
-  background(255);
+  createCanvas(500, 500);
+  div = createP('');
+  div.style('font-family', 'monospace');
 
-  img = loadImage('data/charlize_level.jpg', function(pic){
-    calcImg(pic);
-  });
-}
-
-function draw() {
-  image(capture, 0, 0, width, height);
-  if (live) calcCapture();
-}
-
-function calcImg(pic) {
+  img.loadPixels();
+  let d = pixelDensity();
+  let fullImage = (img.width * d) * (img.height * d);
+  for (let i = 0; i < fullImage; i++) {
+    let r = img.pixels[i*4];
+    let g = img.pixels[i*4+1];
+    let b = img.pixels[i*4+2];
+    
+    let gray = r *0.2126 + g *0.7152 + b *0.0722;
+    
+    img.pixels[i*4] = gray;
+    img.pixels[i*4+1] = gray;
+    img.pixels[i*4+2] = gray;
+  }
+  img.updatePixels();
   var res = '<pre>';
-  for (var i=0; i<60; i++) {
+  for (var i=0; i<img.height; i++) {
     var line = '';
-    for (var j=0; j<140; j++) {
-      var x = pic.get(2+round(j*5.714),5+i*10);
-      var v = round((1-x[0]/255.0)*40);
-      var index = floor(random(options[v].length));
-      var chr = options[v][index];
+    for (var j=0; j<img.width; j++) {
+      let index = (j + i*img.width)*4;
+
+      var x = img.pixels[index];
+
+      var v = round((1-x/255.0)*40);
+      
+      var chr = options[v];
       if (chr==' ') chr='&nbsp;';
       if (chr=='<') chr='&lt;';
       if (chr=='>') chr='&gt;';
@@ -60,30 +49,10 @@ function calcImg(pic) {
     res += line+'<br>';
   }
   res += '</pre>'
-  resdiv.html(res);
-}
+  div.html(res);
+  div.style('font-size', '1px');
+  div.style('letter-spacing', '0.4em');
+  div.style('line-height', '1em')
+  div.position(0, 0);
 
-function calcCapture() {
-  pg.image(capture,0,0,80,60);
-  var res = '<pre>';
-  for (var i=0; i<60; i++) {
-    var line = '';
-    for (var j=0; j<140; j++) {
-      var x = pg.get(round(j*1.143),i*2);
-      var f = (1-x[0]/255.0);
-      f = f*f; //square factor to lighten up, because less bright characters
-      var v = round(f*40);
-      var index = floor(random(options[v].length));
-      var chr = options[v][index];
-      if (chr==' ') chr='&nbsp;';
-      if (chr=='<') chr='&lt;';
-      if (chr=='>') chr='&gt;';
-      if (chr=='"') chr='&quot;';
-      line += chr;
-    }
-
-    res += line+'<br>';
-  }
-  res += '</pre>'
-  resdiv.html(res);
 }
