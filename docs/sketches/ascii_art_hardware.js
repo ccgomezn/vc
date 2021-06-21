cb5a47120df3ef4a5fe18db6c7d2c9f1b36e89fc
ccgomezn <cristhian@voice123.com>
2021-06-21 23:45:20 +0000

    Deploying to gh-pages from  @ bdd8404e21f4ddd6fbbca166fae9151b9f539420 🚀

---
+++ docs/sketches/ascii_art_hardware.js
@@ -0,0 +1,31 @@
+let asciiShader;
+let shaderTexture;
+
+
+function preload() {
+  img = loadImage('/vc/docs/sketches/lenna.png');
+  video = createVideo(['/vc/docs/sketches/fingers.mov', '/vc/docs/sketches/fingers.webm']);
+  asciiShader = loadShader('/vc/docs/sketches/ascii.vert', '/vc/docs/sketches/ascii.frag');
+  video.hide();
+}
+
+function setup() {
+  createCanvas(768, 393, WEBGL);
+  shaderTexture = createGraphics(393, 393, WEBGL);
+  shaderTexture.noStroke();
+
+  video.loop();
+  noStroke();
+}
+
+function draw() {
+  shaderTexture.shader(asciiShader);
+  asciiShader.setUniform('tex', img);
+  texture(shaderTexture);
+  shaderTexture.rect(0,0,393,393);
+  rect(-393,-393/2.0,393,393)
+  asciiShader.setUniform('tex', video);
+  texture(shaderTexture);
+  shaderTexture.rect(0,0,393,393);
+  rect(0,-393/2.0,393,393)
+}
